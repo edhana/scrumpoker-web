7ad9454ed9a67b8bb2cf1e606d26783ae327ff28
Eduardo Marques <edhana@gmail.com>
2019-06-05 22:37:26 +1000

    Add the first routes to the app

--- Makefile
+++ Makefile
@@ -1,17 +1,21 @@
 BUILD := $(shell git rev-parse --short HEAD)
 PROJECTNAME := $(shell basename "$(PWD)")
 
+VERSION := $(shell git describe --tags)
 GOBASE := $(shell pwd)
 GOBIN := $(GOBASE)/bin
 
 # Reserved for future use
-# LDFLAGS=-ldflags "-X=main.Version=$(VERSION) -X=main.Build=$(BUILD)"
+LDFLAGS=-ldflags "-X=main.Version=$(VERSION) -X=main.Build=$(BUILD)"
 
 build:
-	go build -o $(GOBIN)/$(PROJECTNAME) cmd/scrumpoker/main.go
+	go build $(LDFLAGS) -o $(GOBIN)/$(PROJECTNAME) cmd/scrumpoker/main.go
 
 clean:
 	rm -rf $(GOBIN)/*
+
+config:
+	dep ensure -update
 
 default: build
 
--- cmd/scrumpoker/main.go
+++ cmd/scrumpoker/main.go
@@ -2,8 +2,28 @@ package main
 
 import (
 	"fmt"
+	"github.com/apex/log"
+	"github.com/julienschmidt/httprouter"
+	"net/http"
 )
 
+func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	log.Info("Welcome\n")
+}
+
+func Election(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	log.Info("Let's start a new Election\n")
+}
+
 func main() {
-	fmt.Printf("alo mundo\n")
+	log.Info("Initializing the Scrum Poker Server")
+
+	router := httprouter.New()
+	router.GET("/", Index)
+	router.GET("/election/:name", Election)
+
+	err := http.ListenAndServe(":8080", router)
+	if err != nil {
+		log.Fatal(fmt.Sprintf("Error while listening to port 8080: %s", err))
+	}
 }
